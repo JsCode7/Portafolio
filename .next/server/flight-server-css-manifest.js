84277fca3fa788c7c4db1ca4c93341c226d9f332
JsCode7 <josedaniel@Mac-mini-de-Jose.local>
2023-03-26 13:22:14 -0700

    First commit - Portfolio with Next JS13 and TailwindCSS

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_CSS_MANIFEST={"__entry_css__":{"/Users/josedaniel/Desktop/Aplicaciones/ModernWebsite/app/page":["/Users/josedaniel/Desktop/Aplicaciones/ModernWebsite/styles/globals.css"]},"/Users/josedaniel/Desktop/Aplicaciones/ModernWebsite/app/layout.js":["/Users/josedaniel/Desktop/Aplicaciones/ModernWebsite/styles/globals.css"]}
